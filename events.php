f48196846576ed1908af57994b6a4edf65829ffd
Aislinn <design@aislinnkelly.com>
2012-09-14 18:15:59 +0100

    Events page and homepage graphic

---
+++ events.php
@@ -0,0 +1,231 @@
+<?php $subtitle = "Events";
+include($_SERVER['DOCUMENT_ROOT'].'/includes/head.php');		
+include($_SERVER['DOCUMENT_ROOT'].'/includes/header.php')?>	
+	
+		
+	<div class="row primary-row">	
+  		
+  		
+  		
+  		<section role="main">
+  			
+  			
+  			<div class="row primary-container">
+  				<div class="ten columns centered">
+  					<div class="row">
+  					
+  						<div class="twelve columns">
+  						
+  							
+  							<h1>Tradeshows &amp; Events</h1>
+  							<div class="row">
+  							
+  							<p class="seven columns">Praesent eget neque eu eros interdum malesuada non vel leo. Sed fringilla porta ligula egestas tincidunt. Maecenas quis tortor arcu. Vivamus rutrum nunc non neque consectetur.</p>
+  							
+  							<div class="five columns">
+  								<a href="mailto:sales@boxpay.com" class="button right meeting">Schedule a Meeting</a>
+  							</div>
+  							
+  							</div>
+  								
+  						</div>
+  						
+  			<!--			
+  		  				<div class="five columns">
+  		  				
+  		  				
+  		  				
+  		  				</div>	-->	
+  						
+  			
+
+  					
+  					</div>
+  				</div>
+  				
+  				
+  				<div class="twelve columns divider"></div>
+  				
+  				
+  				<div class="ten columns offset-by-one end events ">
+  					<div class="row">
+  					
+  						<div class="twelve columns">
+  					
+  					<h2>Upcoming Events</h2>
+  				
+  					<ul class="no-bullet large">
+  					
+  					  						
+  						<li>
+  							
+  							<h4 class="six">Tokyo Game Show 2012</h4>
+  							<div>
+  							<span class="event-date">September 20 - 21, 2012</span>
+  							<span class="event-location">Tokyo, Japan</span>
+  							</div>
+  						</li>
+  						
+  						
+  						<li>
+  							
+  							<h4 class="six">AppsWorld 2012</h4>
+  							<div>
+  							<span class="event-date">October 2 - 3, 2012</span>
+  							<span class="event-location">London, England</span>
+  							</div>
+  						</li>
+  						
+  						
+  						<li>
+  							
+  							<h4 class="six">WAN-IFRA 2012</h4>
+  							<div>
+  							<span class="event-date">October 29 - 31, 2012</span>
+  							<span class="event-location">Frankfurt, Germany</span>
+  							</div>
+  						</li>
+  						
+  						
+  						<li>
+  							
+  							<h4 class="six">G-Star 2012</h4>
+  							<div>
+  							<span class="event-date">November 8 - 10, 2012</span>
+  							<span class="event-location">Busan, South Korea</span>
+  							</div>
+  						</li>
+
+  					</ul>
+  					
+  				
+  				
+  				
+  				
+  					<h2>Past Events</h2>
+  						  				
+  					<ul class="no-bullet large">
+  							
+  							
+  							
+  								<li>
+  									<h4 class="six">Casual Connect 2012</h4>
+  									<div>
+  									<span class="event-date">July 24 - 26, 2012</span>
+  									<span class="event-location">Seattle, USA</span>
+  									</div>
+  								</li>
+  								
+  								<li>
+  									<h4 class="six">Gamescom 2012</h4>
+  									<div>
+  									<span class="event-date">August 15 - 18, 2012</span>
+  									<span class="event-location">Cologne, Germany</span>
+  									</div>
+  								</li>
+  								
+  								
+  							
+  							
+  								<li>
+  									<h4 class="six">E3 2012</h4>
+  									<div>
+  									<span class="event-date">June 5 - 7, 2012</span>
+  									<span class="event-location">Los Angeles, USA</span>
+  									</div>
+  								</li>
+  								
+  								<li>
+  									<h4 class="six">LA Games Conference 2012</h4>
+  									<div>
+  									<span class="event-date">April 25 - 26, 2012</span>
+  									<span class="event-location">Los Angeles, USA</span>
+  									</div>
+  								</li>
+  								
+  								
+  								<li>
+  									<h4 class="six">GDC 2012</h4>
+  									<div>
+  									<span class="event-date">March 5 - 9, 2012</span>
+  									<span class="event-location">San Francisco, USA</span>
+  									</div>
+  								</li>
+  								
+  								
+  								<li>
+  									<h4 class="six">Mobile World Congress 2012</h4>
+  									<div>
+  									<span class="event-date">February 27 - March 1, 2012</span>
+  									<span class="event-location">Barcelona, Spain</span>
+  									</div>
+  								</li>
+  								
+  								
+  								<li>
+  									<h4 class="six">G-Star 2011</h4>
+  									<div>
+  									<span class="event-date">November 14 - 15, 2011</span>
+  									<span class="event-location">Busan, South Korea</span>
+  									</div>
+  								</li>
+  								
+  								
+  								<li>
+  									<h4 class="six">Social Gaming Summit 2011</h4>
+  									<div>
+  									<span class="event-date">November 14 - 15, 2011</span>
+  									<span class="event-location">London, England</span>
+  									</div>
+  								</li>
+  								
+  								
+  								<li>
+  									<h4 class="six">SocialWest: Monetizing Social Media 2011</h4>
+  									<div>
+  									<span class="event-date">October 20 - 21, 2011</span>
+  									<span class="event-location">San Francisco, USA</span>
+  									</div>
+  								</li>
+  								
+  								
+  								<li>
+  									<h4 class="six">World Telemedia Conference 2011</h4>
+  									<div>
+  									<span class="event-date">October 12 - 14, 2011</span>
+  									<span class="event-location">Amsterdam, Netherlands</span>
+  									</div>
+  								</li>
+  								
+  								<li>
+  									<h4 class="six">NY Games Conference 2011</h4>
+  									<div>
+  									<span class="event-date">September 21 - 22, 2011</span>
+  									<span class="event-location">New York, USA</span>
+  									</div>
+  								</li>
+  				
+  							</ul>
+  						
+  						</div>
+  					</div>
+  				</div>
+  				
+
+  				
+  				
+  				
+  		
+  			
+  		
+  		</section>
+  		
+  		
+  		
+  		
+  		<?php include($_SERVER['DOCUMENT_ROOT'].'/includes/sidebar.php')?>
+		
+	</div><!-- container -->
+			
+ 
+<?php include($_SERVER['DOCUMENT_ROOT'].'/includes/footer.php')?>
